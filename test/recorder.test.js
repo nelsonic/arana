92de6ebfc7c704709c3bbc4cb067a4352a107eb1
Nelson <contact.nelsonic@gmail.com>
2015-08-08 09:25:18 +0100

    [WiP] add_followers method works. just need a couple more failure tests...

--- test/recorder.test.js
+++ test/recorder.test.js
@@ -33,16 +33,23 @@ test.only('Add list of followers to Profile Record', function(t) {
     // at this point the data.entries only has @iteles *real* followers
     // so we'er going to creat a fictional *new* follower to test
     var fictional_follower = 'EverythingIsAwesome' + Math.floor(Math.random()*100000000000);
-    console.log(fictional_follower);
-    // t.ok(data.url === 'https://github.com/iteles', "✓ Profile scraped: "+username);
-    recorder.add_followers(data, function(res){
-      // t.ok(res._version > 0, "✓ Record created: " + res._id);
-      // t.ok(res._type === 'person', "✓ Record type is: " + res._type);
-      console.log(res);
-      t.end();
-    })
-  })
-});
+    // console.log(fictional_follower);
+    data.entries.push(fictional_follower);
+    recorder.add_followers(data, function(res) {
+      var profile = {
+        id:    res._id,
+        index: res._index,
+        type:  res._type
+      }
+      es.read(profile, function(res2){
+        // console.log(res2);
+        var followers = Object.keys(res2._source.followers);
+        t.ok(followers.indexOf(fictional_follower) > -1, "✓ Follower added: " + fictional_follower)
+        t.end();
+      }) // end read
+    }) // end add_followers
+  }) // end scrape for followers list
+}); // end test
 
 // test('Add list of followers to Profile Record', function(t) {
 //   var username = 'iteles';
