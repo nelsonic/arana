55eb030b69965826f70eb1920ee6ec4a257f742f
Nelson <contact.nelsonic@gmail.com>
2015-08-09 23:51:39 +0100

    [WiP] almost there. need to write recorder branch for org repos ...

--- test/recorder.add_repos.test.js
+++ test/recorder.add_repos.test.js
@@ -5,25 +5,53 @@ var es       = require('esta');
 
 // Functional test for recorder.add_people method
 
-test('Add followers crawled BEFORE Profile Record exists', function(t) {
-  var url = 'iteles?tab=repositories';
+test('Add repos crawled BEFORE Profile Record exists', function(t) {
+  var url = 'rjmk?tab=repositories';
   gs(url, function(err, data) {
-    console.log(data);
+    // console.log(data);
     recorder.add_repos(data, function(res) {
-      console.log(res);
+      // console.log(res);
       var profile = {
         id:    res._id,
         index: res._index,
         type:  res._type
       }
       es.read(profile, function(res2) {
-      console.log(" - - - - - - - - - - - - - - - res2:")
+      // console.log(" - - - - - - - - - - - - - - - res2:")
+      // console.log(res2);
+      var r = res2._source.repos.filter(function(repo){
+        return repo.name === 'stopwatch';
+      })
+      r = r[0];
+      // console.log(r);
+      t.ok(res2._source.url === 'https://github.com/rjmk', "✓ Profile: "+res2._source.url)
+      t.ok(r.stars > 0, "✓ "+url +" has " +res2._source.repos.length + " repos" )
+      t.end();
+      }) // end read
+    }) // end add_repos
+  }) // end scrape for followers list
+}); // end test
+
+
+test('Add repos to EXISTING Profile Record (created in add_people test)', function(t) {
+  var url = 'iteles?tab=repositories';
+  gs(url, function(err, data) {
+    // console.log(data);
+    recorder.add_repos(data, function(res) {
+      // console.log(res);
+      var profile = {
+        id:    res._id,
+        index: res._index,
+        type:  res._type
+      }
+      es.read(profile, function(res2) {
+      // console.log(" - - - - - - - - - - - - - - - res2:")
       // console.log(res2);
       var r = res2._source.repos.filter(function(repo){
         return repo.name === 'talks';
       })
       r = r[0];
-      console.log(r);
+      // console.log(r);
       t.ok(res2._source.url === 'https://github.com/iteles', "✓ Profile: "+res2._source.url)
       t.ok(r.stars > 0, "✓ "+url +" has " +res2._source.repos.length + " repos" )
       t.end();
--- test/recorder.test.js
+++ test/recorder.test.js
@@ -66,3 +66,30 @@ test('RECORDER save ORG page data', function(t) {
     })
   })
 });
+
+test('RECORD repos to EXISTING Profile Record', function(t) {
+  var url = 'iteles?tab=repositories';
+  gs(url, function(err, data) {
+    // console.log(data);
+    recorder(data, function(res) {
+      // console.log(res);
+      var profile = {
+        id:    res._id,
+        index: res._index,
+        type:  res._type
+      }
+      es.read(profile, function(res2) {
+      // console.log(" - - - - - - - - - - - - - - - res2:")
+      // console.log(res2);
+      var r = res2._source.repos.filter(function(repo){
+        return repo.name === 'talks';
+      })
+      r = r[0];
+      // console.log(r);
+      t.ok(res2._source.url === 'https://github.com/iteles', "✓ Profile: "+res2._source.url)
+      t.ok(r.stars > 0, "✓ "+url +" has " +res2._source.repos.length + " repos" )
+      t.end();
+      }) // end read
+    }) // end add_repos
+  }) // end scrape for followers list
+}); // end test
