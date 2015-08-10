11f0b5a123abbc2580454bc5dc70774a556e7e19
Nelson <contact.nelsonic@gmail.com>
2015-08-10 22:40:38 +0100

    issues scraper works exactly as required. https://youtu.be/8iwBM_YB1sE?t=59s :tada:

--- test/recorder.add_issues.test.js
+++ test/recorder.add_issues.test.js
@@ -19,7 +19,8 @@ test('RECORDER save List of ISSUEs to Repo Record', function(t) {
       es.read(repo, function(res2) {
         // console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
         // console.log(res2._source.issues);
-        t.ok(res2._source.issues.length > 1, "✓ "+url +" has " +res2._source.issues.length + " issues!" )
+        var count = res2._source.issues.length
+        t.ok(count > 1, "✓ "+url +" has " +count + " issues!" )
         t.end();
       }) // end read
     }) // end add_issues
@@ -31,18 +32,24 @@ test('RECORDER save SECOND PAGE of ISSUEs to Repo Record', function(t) {
   gs(url, function(err, data) {
     // console.log(data);
     recorder.add_issues(data, function(res) {
-      // console.log(res);
-      var repo = {
-        id:    res._id,
-        index: res._index,
-        type:  res._type
-      }
-      es.read(repo, function(res2) {
-        // console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
-        // console.log(res2._source.issues);
-        t.ok(res2._source.issues.length > 1, "✓ "+url +" has " +res2._source.issues.length + " issues!" )
-        t.end();
-      }) // end read
+      gs(data.next_page, function(err2, data2){
+        recorder(data2, function(res2){
+          // console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
+          // console.log(res2);
+          var repo = {
+            id:    res2._id,
+            index: res2._index,
+            type:  res2._type
+          }
+          es.read(repo, function(res3) {
+            // console.log(' - - - - - - - - - - - - - - - - - - - - - - res3:')
+            // console.log(res3._source);
+            var count = res3._source.issues.length
+            t.ok(count > 1, "✓ "+ res3._source.url +" has " +count + " issues!" )
+            t.end();
+          }) // end read
+        })
+      })
     }) // end add_issues
   }) // end scrape for followers list
 }); // end test
