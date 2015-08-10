13a8abce6708470e3c567f0309f8bf763b5f0b9d
Nelson <contact.nelsonic@gmail.com>
2015-08-10 07:26:09 +0100

    recorder saves a repo. no sweat. fixes https://github.com/nelsonic/arana/issues/9

---
+++ test/recorder.save_repo.test.js
@@ -0,0 +1,28 @@
+var test     = require('tape');
+var recorder = require('../lib/recorder');
+var gs       = require('github-scraper');
+var es       = require('esta');
+
+// Functional test for recorder.add_people method
+
+test('RECORDER save repository (save-all-the-things method!)', function(t) {
+  var url = 'nelsonic/practical-js-tdd';
+  gs(url, function(err, data) {
+    // console.log(data);
+    recorder(data, function(res) {
+      // console.log(res);
+      var repo = {
+        id:    res._id,
+        index: res._index,
+        type:  res._type
+      }
+      es.read(repo, function(res2) {
+        console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
+        console.log(res2);
+        t.ok(res2._source.url.indexOf(url) > -1, "✓ Repo URL: "+res2._source.url)
+        t.ok(res2._source.stars > 0, "✓ "+url +" has " +res2._source.stars + " stars" )
+        t.end();
+      }) // end read
+    }) // end save_repo
+  }) // end scrape for followers list
+}); // end test
