fb15e87a9b57d8686fe4468e4306daadcb135353
Nelson <contact.nelsonic@gmail.com>
2015-08-11 21:35:16 +0100

    adds worker test for 302 error. see tests/worker.test.js fixes https://github.com/nelsonic/arana/issues/15

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 var worker = require('./lib/worker');
 var wq     = require('./lib/work_queue');
+var gs     = require('github-scraper')
+var recorder = require('./lib/recorder');
+var tasker = require('./lib/tasker')
+var concurrency_level = 3
+var task = '/iteles'+ " "+Date.now() + " " + Date.now()+1
+var url = task.split(' ')[0];
+wq.add(url, function(err, data) { // initial task
 
-wq.add('/nelsonic', function(err, data) { // initial task
-  worker(function(res){
-    console.log(res)
-    console.log(" - - - - - - - - end - - - - - - - ");
-  });
+  gs(url, function(err, data){
+    recorder(data, function(res){
+      console.log(res);
+      var tasks = tasker(data);
+      console.log(tasks);
+      var count = tasks.length
+      tasks.map(function(t){
+        wq.add(t, function(e, d){
+          // console.log(e, d);
+        })
+      });
+
+      wq.finish(task, function(err2, data2){
+        console.log(err2, data2);
+        boot();
+      })
+    });
+    // return true;
+  })
 })
+
+function boot(){
+  setInterval(function() {
+    // check how many workers are currently running
+    wq.count('in-progress', function(err2, count) {
+      // if the in-progress count is less than concurrency_level spawn
+      if(count < concurrency_level){
+        // for(var i = concurrency_level - count; i >= 0; i--){
+          worker(function(res){
+            console.log(res)
+            console.log(" - - - - - - - - end - - - - - - - ");
+          });
+        // }
+      }
+    })
+  },300);
+}
--- test/worker.test.js
+++ test/worker.test.js
@@ -1,12 +1,33 @@
 // var worker = require('../lib/worker');
-// var test = require('tape');
+// var wq     = require('../lib/work_queue');
+// var test   = require('tape');
 //
-// test("worker scrapes page and adds task to work queue", function(t){
-//   worker(function(res){
-//     console.log(res)
-//     console.log(" - - - - - - - - end - - - - - - - ");
-//     t.ok(res.created === true, "✓ record created: "+res._id)
-//     worker.redisClient.end();
-//     t.end();
+// test("Simulate Error by adding task far in the past and scraping", function(t){
+//   var task = 'ladieswhocode/london-tech-event-hack-collection/issues';
+//   var day = 24 * 60 * 60 * 1000
+//   var yesterday = Date.now() - day;
+//   console.log(new Date(yesterday));
+//   var args = ["work-queue", yesterday, task]
+//   console.log(args)
+//   worker.redisClient.zadd(args, function (err, data) {
+//     // console.log(err, data)
+//     worker(function(res) {
+//       // console.log(res)
+//       t.ok(res === 302, "✓ Handled error for REPO WITHOUT Issues: "+res)
+//       t.end();
+//     });
 //   });
+// })
+//
+// test("worker scrapes page and adds task to work queue", function(t) {
+//   var task = '/iteles';
+//   wq.add(task, function(err, data){
+//     worker(function(res){
+//       console.log(res)
+//       console.log(" - - - - - - - - end - - - - - - - ");
+//       t.ok(res.created === true, "✓ Record created: "+res._id)
+//       worker.redisClient.end();
+//       t.end();
+//     });
+//   })
 // })
