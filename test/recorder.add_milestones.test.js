f64981f6d98bcb2c2c6a2da986f8975329421df6
Nelson <contact.nelsonic@gmail.com>
2015-08-12 00:22:18 +0100

    recorder now saves milestones and labels to repo record. tasker also adds tasks for /labels and /milestones. fixes https://github.com/nelsonic/arana/issues/17 https://github.com/nelsonic/arana/issues/18

---
+++ test/recorder.add_milestones.test.js
@@ -0,0 +1,51 @@
+var test     = require('tape');
+var recorder = require('../lib/recorder');
+var gs       = require('github-scraper');
+var es       = require('esta');
+
+// Functional test for recorder.add_people method
+
+test('RECORDER save List of MILESTONES to Repo Record', function(t) {
+  var url = 'https://github.com/dwyl/tudo/milestones';
+  gs(url, function(err, data) {
+    // console.log(data);
+    recorder.add_milestones(data, function(res) {
+      // console.log(res);
+      var repo = {
+        id:    res._id,
+        index: res._index,
+        type:  res._type
+      }
+      es.read(repo, function(res2) {
+        // console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
+        // console.log(res2._source);
+        var count = res2._source.milestones.length
+        t.ok(count > 1, "✓ "+url +" has " +count + " milestones!" )
+        t.end();
+      }) // end read
+    }) // end add_issues
+  }) // end scrape for followers list
+}); // end test
+
+test('RECORDER save MILESTONES to record again (in-direct)', function(t) {
+  var url = 'dwyl/tudo/milestones';
+  gs(url, function(err, data) {
+    // console.log(data);
+    recorder(data, function(res2) {
+      // console.log(' - - - - - - - - - - - - - - - - - - - - - - res2:')
+      // console.log(res2);
+      var repo = {
+        id:    res2._id,
+        index: res2._index,
+        type:  res2._type
+      }
+      es.read(repo, function(res3) {
+        // console.log(' - - - - - - - - - - - - - - - - - - - - - - res3:')
+        // console.log(res3._source);
+        var count = res3._source.milestones.length
+        t.ok(count > 1, "✓ "+ res3._source.url +" has " +count + " milestones" )
+        t.end();
+      }) // end read
+    }) // end add_issues
+  }) // end scrape for followers list
+}); // end test
